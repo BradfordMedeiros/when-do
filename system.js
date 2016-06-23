ed1528bdc93e568bce836cb17d1b6dab7b8900ee
Bradford Medeiros <bradmedeiros0@gmail.com>
2016-06-23 14:21:21 -0400

    Add files via upload

--- system.js
+++ system.js
@@ -1,11 +1,13 @@
 
 var child_process = require("child_process");
+var path = require("path");
 
-var action = function (path){
+
+var action = function (the_path){
 	
 	var self = this;
 	
-	this.path = path;
+	this.path = path.resolve(the_path);
 	this.execute = function(){
 		child_process.execFile(self.path,(err,stdout,stderr)=>{
 			console.log('stdout:  '+stdout);
@@ -16,34 +18,47 @@ var action = function (path){
 };
 
 
-
 // figure out best way to handle this
 // state should be valid json (so then we can do easy number comparisons)
-var state = function (path, callback){
+// maybe allow just a simple json representation instead of an executable as well
+// x.state or x.state.y or x.state.json
+var state = function (the_path){
 	
 	var self = this;
-	this.path = path;
+	this.path = path.resolve(the_path);
 	
-	this.get_state = function(){
-		
+	// returns a promise
+	this.get_state = function(){	
 		var promise = new Promise(function(resolve,reject){
-			
 			child_process.execFile(self.path,(err,stdout,stderr)=>{
-			console.log('stdout:  '+stdout);
-			console.log('error:  '+err);
-			console.log('stderr: '+stderr);
-			var result = stdout;
-			resolve(result);
-			
+				console.log('stdout:  '+stdout);
+				console.log('error:  ('+err+")");
+				console.log('$'+typeof(err));
+				console.log('stderr: '+stderr);
+
+				if (err === null){
+					resolve(stdout)
+				}else{
+					reject(stderr);
+				}
+			});
 		});
-		
-		promise.resolve(()=>{
-			return 
-				
-		});
-		
+		return promise;
 	};
 };
+
+var condition = function(path){
+	
+	var self = this;
+	
+	
+};
+
+// returns condition based upon the json
+function convert_json_to_condition(json_condition){
+	
+	
+}
 
 var system = function(actions, state,conditions){
 	
@@ -53,17 +68,38 @@ var system = function(actions, state,conditions){
 	
 };
 
+/// condition
+/* 
+{
+	data: [temp, humidity] --> each assumes this topics exist
+	
+	
+	eval: 
+		(temp,humdity)=> {
+			temp > hum
+	
+	options: {
+		rate:  x,
+		action_limit: y
+		etc
+	}
+	
+	
+}
+*/
+//////////
+
 
 // generates a system based upon the path to the root folder
 // make into a promise
 // need to do a recursive walk of the file system
 // make it easy and just name the files x.action.y or x.action
 
-var load_system from_path = function(sys_when_do_root_folder, callback){
+var load_system_from_path = function(sys_when_do_root_folder, callback){
 	
-	var actions = load_actions_path(sys_when_do_root_folder+'/actions');
-	var states = load_states_path (sys_when_do_root_folder+'/state');
-	var conditions = load_conditions_path(sys_condition_folder+'/conditions');
+	var actions = load_actions_path(path.resolve(sys_when_do_root_folder,'actions'));
+	var states = load_states_path (path.resolve(sys_when_do_root_folder,'state'));
+	var conditions = load_conditions_path(path.resolve(sys_condition_folder,'conditions'));
 	var new_system = new system(actions,conditions);
 	callback(new_system);
 	
@@ -92,3 +128,8 @@ var load_actions_path = function(sys_condition_folder){
 };
 
 
+module.exports = {
+	action: action,
+	state: state
+	
+};
