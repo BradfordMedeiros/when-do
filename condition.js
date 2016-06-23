30a54df7fa948de36c757e7d7ecb321b51af6b3c
bradmedeiros <bradmedeiros0@gmail.com>
2016-06-22 20:25:30 -0400

    -

--- condition.js
+++ condition.js
@@ -3,17 +3,18 @@
 //logic.when( logic.and(1,humidity_is_high, it_is_night)).do(x)
 
 
-var condition = function(func, get_value){
-    this.get_value = get_value;
-    this.eval = ()=> this.get_value();
+var condition = function(get_value){
+    this.eval = ()=> get_value();
 };
 condition.prototype.type = 'condition';
 
 var and_condition = function(condition0,condition1){
+
+    var args = arguments;
     this.eval = ()=>{
         var is_true = true;
-        for (var i = 0 ; i < arguments.length; i++){
-            if (!arguments[i].eval()){
+        for (var i = 0 ; i < args.length; i++){
+            if (!args[i].eval()){
                 is_true = false;
                 break;
             }
@@ -29,13 +30,11 @@ var or_condition = function(condition0,conditionn){
     if (arguments.length === 0){
         throw (new Error("or condition must contain at least one condition"));
     }
-    
+  
     this.args = arguments;
     this.eval = ()=>{
-        console.log(this.args);
         for (var i = 0 ; i < this.args.length; i++){
             var value = this.args[i].eval();
-            console.log('value is '+value);
             if (value === true){
                 is_true = true
                 break;
@@ -46,8 +45,24 @@ var or_condition = function(condition0,conditionn){
 };
 or_condition.prototype.type = 'or_condition';
 
+
+var    val0 =  {val:true}
+var    val1 =  {val:true}
+var    f0 = new condition(()=>val0.val)
+var        f1=  new condition(()=>val1.val)
+var        oor= new or_condition(f0,f1)
+var        aand= new and_condition(f0,f1)
+
 module.exports = {
     condition: condition,
     and: and_condition,
-    or: or_condition
+    or: or_condition,
+    test: {
+        o: oor,
+        a: aand,
+        v0: val0,
+        v1: val1,
+        f0:f0,
+        f1: f1
+    }
 };
