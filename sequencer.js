774d95bcce86c72b58cc76de1d24c6043eda9be3
bradmedeiros <bradmedeiros0@gmail.com>
2016-09-08 01:00:04 -0400

    added overall promise for sequences, only run once functionality

--- sequencer.js
+++ sequencer.js
@@ -1,9 +1,10 @@
 // seq.then(func).wait(1000).then(func1).loop(func2,1000)
 
-
+// need to do better error handling with the promises.  behavior of good cases is pretty nice tho
 
 var sequencer = function(){
 	this.the_events=  [ ];
+    this.has_been_run = false;
 };
 var event = function(value, type){
 	this.value = value;
@@ -20,6 +21,8 @@ function create_time_promise (time){
 /*
     If the action is a promise we will wait until the promise resolves before continuing!
 */ 
+
+// should add some error handling here and pass up errors
 function create_action_promise (action){
 	var value = action();
     
@@ -52,16 +55,40 @@ function create_promise(event){
     return promise_creation_function(event.value);
 }
 
-function attach_promise ( promise, index,events){
-	promise.then(()=>{
-		var the_promise = create_promise(events[index]);
-		the_promise.then(()=>{
-			if (index < events.length){
-				attach_promise(the_promise, index+1, events);
-			}		
-		});
-	})
+function attach_promise ( promise, index, events, resolve, reject){
+  
+    if (index >= events.length){
+        resolve();
+    }else{
+        promise.then(()=>{
+            if (index < events.length){
+                var the_promise = create_promise(events[index]);
+                the_promise.then(()=>{
+                    attach_promise(the_promise,index+1, events,resolve,reject)
+                }).catch((x)=>reject(x));
+            }
+        });    
+        promise.catch((x)=>reject(x)); 
+    }
 }
+
+
+//if func returns a promise, we wait for that promise to be satisfied before moving on
+sequencer.prototype.then = function(func){
+	if (typeof(func) !== typeof(func)){
+		throw (new Error("func in then must be function, provided: ",typeof(func)));
+	}
+	this.the_events.push(new event(func,'then'));
+	return this;
+};
+
+sequencer.prototype.wait = function(time){
+	if (typeof (time) !== typeof (1)){
+		throw (new Error("time in wait must be of type number, provided type ",typeof(time)));
+	}
+	this.the_events.push(new event(time,'wait'));
+	return this;
+};
 
 sequencer.prototype.loop = function(func, times, delay_in_ms){
 
@@ -86,30 +113,26 @@ sequencer.prototype.loop = function(func, times, delay_in_ms){
 	return this;
 };
 
-//if func returns a promise, we wait for that promise to be satisfied before moving on
-sequencer.prototype.then = function(func){
-	if (typeof(func) !== typeof(func)){
-		throw (new Error("func in then must be function, provided: ",typeof(func)));
-	}
-	this.the_events.push(new event(func,'then'));
-	return this;
-};
-
-sequencer.prototype.wait = function(time){
-	if (typeof (time) !== typeof (1)){
-		throw (new Error("time in wait must be of type number, provided type ",typeof(time)));
-	}
-	this.the_events.push(new event(time,'wait'));
-	return this;
-};
-
 sequencer.prototype.run = function(){
-	
-	var last_promise = undefined;
-	last_promise = create_promise(this.the_events[0]);
-	attach_promise(last_promise, 1,this.the_events);	
-	this.the_events = [ ];
+    if (this.has_been_run === true){
+        throw (new Error("a sequence can only be ran once"));
+    }
+	this.has_been_run = true;
+    
+    var the_resolve = undefined;
+    var the_reject = undefined;
+    var sequence_promise = new Promise(function(resolve,reject){
+        the_resolve = resolve;
+        the_reject = reject;
+    });
+    
+    if (the_resolve === undefined || the_reject === undefined){
+        throw (new Error("this should never happen-- only in here for testing, i don't think this code works like this will research but putting here in case this happens"));
+    }
+	//var last_promise = undefined;
+	//last_promise = create_promise(this.the_events[0]);
+	attach_promise(new Promise(function(resolve,reject){resolve()}), 0,this.the_events,the_resolve,the_reject);	
+	//this.the_events = [ ]; // i think this could cause issues 
+    return sequence_promise;
 };
-
-
 module.exports = sequencer;
