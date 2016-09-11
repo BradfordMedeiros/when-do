19d0a1067a37cfbb84a4240438e50f6ee44a422d
bradmedeiros <bradmedeiros0@gmail.com>
2016-09-10 23:28:34 -0400

    started separating out system cause that file is gonna get so shitty and messy

---
+++ action.js
@@ -0,0 +1,111 @@
+
+var child_process = require("child_process");
+var path = require("path");
+var fse = require("fs-extra");
+
+var action = function (the_path){
+	
+	var self = this;
+	
+	this.path = path.resolve(the_path);
+	// returns a promise
+	this.execute = (value)=>generate_action_promise(the_path,value);
+    
+    this.get_name = function(state_name){
+        // we consider the name the filename but not including the extension (.action.*)
+        var base_name = path.basename(this.path);
+        return base_name.slice(0,base_name.lastIndexOf('.action.')); 
+    };
+};
+
+action.is_action = function(action_path){
+    return is_identifier(action_path,'action');
+};
+
+var load_actions_path = function(sys_condition_folder){
+	var actions = [ ];
+    
+    var promise = new Promise(function(resolve,reject){
+        fse.walk(sys_condition_folder).on('data',(file)=>{            
+            if (action.is_action(file.path)){
+                console.log('added action:  '+file.path);
+                actions.push(new action(file.path));
+            }
+        }).on('end',()=>{
+            console.log('done adding actions');
+            resolve(actions);
+        });
+    });
+	return promise;
+};
+
+function is_identifier(path_to_file,type){
+    if (type !== 'action' && type!== 'state' && type!=='condition'){
+        throw (new Error("Type: "+type+" is an invalid identifier"));
+    }
+    var the_file = path.basename(path_to_file);
+    var state = the_file.split('.');
+    
+    return (
+        (the_file[0] !== '.') && 
+        (the_file[the_file.length-1] !== '.') && 
+        state[state.length-2] === type && 
+        state.length-2 >0
+        );
+
+};
+
+
+var generate_action_promise = function(the_path, json_value){
+    var parts = the_path.split(".");
+    var is_json = parts[parts.length-1] === "json";
+    
+    var the_promise = undefined;
+
+    if (is_json){       
+        console.log("writing json file");
+            
+        var the_json_value = json_value !== undefined? JSON.stringify(json_value): "0";
+        var command = "echo "+the_json_value+" > "+the_path;
+        console.log("calling ", command);
+        // this should be changed eventually but should be fine for now
+        child_process.exec(command,function(err){
+            if (err){
+                console.log(err);
+                console.log("warning error");
+            }else{
+                console.log("finished writing file ",the_path);
+            }
+        });
+    }else{
+        the_promise = new Promise(function(resolve,reject){
+        
+            var parameter = json_value === undefined? '': JSON.stringify(json_value);
+            var command = the_path + ' ' + parameter;
+			child_process.exec(
+            command,
+            {cwd:the_path+'/..'},
+            (err,stdout,stderr)=>{
+
+				var is_error = false;
+                try{
+                    var json_result = JSON.parse(stdout);
+                   
+                }catch(e){
+                    is_error = true;
+                }
+				if (err === null && !is_error){
+                    console.log("Finished executing action success ",the_path);
+					resolve(json_result)
+                }else{
+                    console.log("Error executing action success ",the_path);
+                    console.log("expected json got ",stdout);
+                    reject(stderr);
+                }
+			});
+		});
+        return the_promise;
+    }
+}
+
+module.exports = load_actions_path;
--- system.js
+++ system.js
@@ -4,24 +4,6 @@ var child_process = require("child_process");
 var path = require("path");
 var fse = require("fs-extra");
 
-var action = function (the_path){
-	
-	var self = this;
-	
-	this.path = path.resolve(the_path);
-	// returns a promise
-	this.execute = (value)=>generate_action_promise(the_path,value);
-    
-    this.get_name = function(state_name){
-        // we consider the name the filename but not including the extension (.action.*)
-        var base_name = path.basename(this.path);
-        return base_name.slice(0,base_name.lastIndexOf('.action.')); 
-    };
-};
-
-action.is_action = function(action_path){
-    return is_identifier(action_path,'action');
-};
 
 // figure out best way to handle this
 // state should be valid json (so then we can do easy number comparisons)
@@ -57,6 +39,8 @@ function is_identifier(path_to_file,type){
         );
 
 };
+
+
 state.is_state = function(state_path){
     return is_identifier(state_path,'state');
 };
@@ -199,6 +183,8 @@ var system = function(actions,states,conditions){
 // need to do a recursive walk of the file system
 // make it easy and just name the files x.action.y or x.action
 
+var load_actions_path = require("./action.js");
+
 var load_system_from_path = function(sys_when_do_root_folder){
 
     var resolver = undefined;
@@ -273,22 +259,6 @@ var load_states_path = function(sys_condition_folder){
 	return promise;
 }
 
-var load_actions_path = function(sys_condition_folder){
-	var actions = [ ];
-    
-    var promise = new Promise(function(resolve,reject){
-        fse.walk(sys_condition_folder).on('data',(file)=>{            
-            if (action.is_action(file.path)){
-                console.log('added action:  '+file.path);
-                actions.push(new action(file.path));
-            }
-        }).on('end',()=>{
-            console.log('done adding actions');
-            resolve(actions);
-        });
-    });
-	return promise;
-};
 
 // Returns json result from the file
 // Executes file if it's executable, else reads it
@@ -344,60 +314,6 @@ var generate_state_promise = function(the_path){
     return the_promise;
 
 };
-
-
-var generate_action_promise = function(the_path, json_value){
-    var parts = the_path.split(".");
-    var is_json = parts[parts.length-1] === "json";
-    
-    var the_promise = undefined;
-
-    if (is_json){       
-        console.log("writing json file");
-            
-        var the_json_value = json_value !== undefined? JSON.stringify(json_value): "0";
-        var command = "echo "+the_json_value+" > "+the_path;
-        console.log("calling ", command);
-        // this should be changed eventually but should be fine for now
-        child_process.exec(command,function(err){
-            if (err){
-                console.log(err);
-                console.log("warning error");
-            }else{
-                console.log("finished writing file ",the_path);
-            }
-        });
-    }else{
-        the_promise = new Promise(function(resolve,reject){
-        
-            var parameter = json_value === undefined? '': JSON.stringify(json_value);
-            var command = the_path + ' ' + parameter;
-			child_process.exec(
-            command,
-            {cwd:the_path+'/..'},
-            (err,stdout,stderr)=>{
-
-				var is_error = false;
-                try{
-                    var json_result = JSON.parse(stdout);
-                   
-                }catch(e){
-                    is_error = true;
-                }
-				if (err === null && !is_error){
-                    console.log("Finished executing action success ",the_path);
-					resolve(json_result)
-                }else{
-                    console.log("Error executing action success ",the_path);
-                    console.log("expected json got ",stdout);
-                    reject(stderr);
-
-                }
-			});
-		});
-        return the_promise;
-    }
-}
 
 
 module.exports = load_system_from_path;
